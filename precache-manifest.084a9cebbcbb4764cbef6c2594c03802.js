5754f8a2c1d7c95ad48825ab411f979b9059b718
mefa <mfbyrktr@gmail.com>
2020-03-11 22:17:13 +0300

    Updates

---
+++ precache-manifest.084a9cebbcbb4764cbef6c2594c03802.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063be7dacf447a811f1ed9b513685f77",
+    "url": "/formula1/index.html"
+  },
+  {
+    "revision": "d967d962fc4eabf01d80",
+    "url": "/formula1/static/css/main.af63dcd3.chunk.css"
+  },
+  {
+    "revision": "7223757774501bb257b1",
+    "url": "/formula1/static/js/2.350eac61.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/formula1/static/js/2.350eac61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d967d962fc4eabf01d80",
+    "url": "/formula1/static/js/main.985a0b32.chunk.js"
+  },
+  {
+    "revision": "a6ce7d06f0a96ab4f253",
+    "url": "/formula1/static/js/runtime-main.1c0ac6a8.js"
+  },
+  {
+    "revision": "94135fc8508b66cabd46423f3fffa571",
+    "url": "/formula1/static/media/background_min.94135fc8.jpeg"
+  }
+]);
