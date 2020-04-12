d78454485efabd04309155cf3a123721dd9948f9
mefa <mfbyrktr@gmail.com>
2020-04-12 21:04:49 +0300

    visual

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import HeaderUnit from "./components/Header.unit/Header.unit.js"
 import NextRaceUnit from './components/NextRace.unit/NextRace.unit';
+import CarAnimation from './components/CarAnimation.unit/CarAnimation.unit';
 
 function App() {
 
 
   return (
-    <div className="App">
+    <div >
       <HeaderUnit />
-      <NextRaceUnit />
+      <CarAnimation />
+      <div className="App">
+        <NextRaceUnit />
+      </div>
     </div>
   );
 }
